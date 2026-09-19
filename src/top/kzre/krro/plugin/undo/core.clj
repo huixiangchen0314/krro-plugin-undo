5b329ee42eb47b884ca48e240cd25bdfe302bf1b
lyt0628 <LYT.0628@qq.com>
2026-09-20 07:03:34 +0800

    1. add fx command

--- src/top/kzre/krro/plugin/undo/core.clj
+++ src/top/kzre/krro/plugin/undo/core.clj
@@ -2,34 +2,52 @@
 (ns top.kzre.krro.plugin.undo.core
   "Undo 插件注册入口，集成 hook 通知。"
   (:require
-   [top.kzre.krro.core.core :as krro ]
-   [top.kzre.krro.core.hook :as hook]
-   [top.kzre.krro.core.project :as proj]
-   [top.kzre.krro.core.resource :as res]
-   [top.kzre.krro.plugin.undo.internal.impl :as impl]
-   [top.kzre.krro.plugin.undo.internal.project :as undo-proj]
-   [top.kzre.krro.plugin.undo.plugin :as undo-cmd]
-   [top.kzre.krro.plugin.undo.protocol :as proto]))
+    [clojure.spec.alpha :as s]
+    [top.kzre.krro.core.core :as krro]
+    [top.kzre.krro.core.project :as proj]
+    [top.kzre.krro.core.resource :as res]
+    [top.kzre.krro.plugin.undo.internal.impl :as impl]
+    [top.kzre.krro.plugin.undo.internal.project :as undo-proj]
+    [top.kzre.krro.plugin.undo.protocol :as proto]))
+
+(s/def ::fx-type keyword?)
+
+(defn- make-fx-metadata [type data]
+  (assoc data ::fx-type type))
+
+(defrecord UndoCommand [undo-handler
+                        redo-handler])
+
+(defonce ^:private fx-command (atom {}))
+
+(defn reg-undo-fx [id undo-handler redo-handler]
+  {:pre [(keyword? id)
+         (ifn? undo-handler)
+         (ifn? redo-handler)]}
+  (swap! fx-command assoc id (->UndoCommand undo-handler redo-handler)))
+
+(defn- get-command [id]
+  (get @fx-command id))
+
+(defn- fx-type [node]
+  (get (proto/metadata node) ::fx-type))
 
 ;; 手动操作api
 
 (defn record-state!
   ([] (record-state! nil))
-  ([metadata]
+  ([data]
    (swap! proj/project
           (fn [project]
             (let [current (or (:krro.undo/undo-tree project)
                               (impl/make-undo-tree (res/encode (proj/user-data project))))]
               (assoc project :krro.undo/undo-tree
-                             (proto/add-state! current (res/encode (proj/user-data project)) metadata)))))))
+                             (proto/add-state! current (res/encode (proj/user-data project)) data)))))))
 
-(defn record-undo-command!
-  [type metadata]
-  (record-state! (undo-cmd/make-metadata type metadata)))
+(defn record-fx!
+  [type data]
+  (record-state! (make-fx-metadata type data)))
 
-
-(defn- record-state-handler [_project]
-  (record-state!))
 
 (defn- restore-state [ current-node]
   (let [protected (proj/protected-data)]
@@ -40,19 +58,20 @@
     (let [new-current (proto/undo current)]
       (if (identical? new-current current)
         project
-        (do (hook/run-hook! :krro.undo/before-undo-hook
-                            {:old-project project
-                             :new-project nil
-                             :old-node current
-                             :new-node new-current})
-            (let [new-project (restore-state new-current)]
-              (swap! proj/project (constantly new-project))
-              (hook/run-hook! :krro.undo/after-undo-hook
-                              {:old-project project
-                               :new-project new-project
-                               :old-node current
-                               :new-node new-current})
-              new-project))))
+        (let [new-project (restore-state new-current)]
+          (letfn [(go [] (swap! proj/project (constantly new-project)))]
+            ;; 从当前节点 undo
+            (if-let [fx (fx-type current)]
+              (if-let [cmd (get-command fx)]
+                ((:undo-handler cmd)
+                 {:old-project project
+                  :new-project new-project
+                  :old-node current
+                  :new-node new-current}
+                 go)
+                (throw (ex-info "no command registered for fx-type" {:fx-type fx})))
+              (go)))
+          new-project)))
     project))
 
 (defn- redo-handler [project]
@@ -60,21 +79,20 @@
     (let [new-current (proto/redo current)]
       (if (identical? new-current current)
         project
-        (do
-          (hook/run-hook! :krro.undo/before-redo-hook
-                          {:old-project project
-                           :new-project nil
-                           :old-node current
-                           :new-node new-current})
-          (let [new-project (restore-state new-current)]
-            (swap! proj/project (constantly new-project))
-            ;; after：在 swap! 之后触发
-            (hook/run-hook! :krro.undo/after-redo-hook
-                            {:old-project project
-                             :new-project new-project
-                             :old-node current
-                             :new-node new-current})
-            new-project))))
+        (let [new-project (restore-state new-current)]
+          (letfn [(go [] (swap! proj/project (constantly new-project)))]
+            ;; 从当前节点 undo
+            (if-let [fx (fx-type new-current)]
+              (if-let [cmd (get-command fx)]
+                ((:redo-handler cmd)
+                 {:old-project project
+                  :new-project new-project
+                  :old-node current
+                  :new-node new-current}
+                 go)
+                (throw (ex-info "no command registered for fx-type" {:fx-type fx})))
+              (go)))
+          new-project)))
     project))
 
 
@@ -107,7 +125,6 @@
      (undo-proj/polyfill-undo-tree)
      (krro/reg-command :krro.undo/undo undo-handler :description "Undo last change")
      (krro/reg-command :krro.undo/redo redo-handler :description "Redo last undone change")
-     (krro/reg-command :krro.undo/record-state record-state-handler :description "Save current state to undo tree")
      (krro/reg-command :krro.undo/undo-switch-branch switch-branch-handler
                             :description "Switch to a different undo branch"
                             :interactive [[:choice branch-options]])
@@ -117,9 +134,5 @@
        :keymap
        {:u :krro.undo/undo
         :r :krro.undo/redo
-        })
-
-     ;; undo 插件安装的子插件系统
-     (undo-cmd/mount)
-     )})
+        }))})
 
